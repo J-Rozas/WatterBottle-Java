441d4c575a749c93c2d7c0a96e4ebe3c30c8f737
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-13 19:49:12 +0100

    Add initial setup for tests for class WaterBottle

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+    WaterBottle bottle;
+
+    @Before
+    public void before() {
+        bottle = new WaterBottle;
+    }
+
+}
